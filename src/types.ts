2faee74cf3309939e77c2ec021bc60aff42b377d
Hyunwoo Kim <admin@hyunwoo.kim>
2023-01-18 09:22:48 +0900

    Organize Type Declarations

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,28 @@
+export enum EventType {
+  Airplane = 'Airplane',
+  Bus = 'Bus',
+  Train = 'Train',
+  Transfer = 'Transfer',
+  Eat = 'Eat',
+  Place = 'Place',
+  Sleep = 'Sleep',
+}
+
+export interface Collection {
+  title: string | null;
+  subtitle: string | null;
+  events: Event[];
+}
+
+export interface Event {
+  date: Date | string | null;
+  title: string;
+  subtitle: string;
+  description: string;
+  lat: number | null;
+  lng: number | null;
+}
+
 enum SortBy {
   DISTANCE = 'distance',
   ACCURACY = 'accuracy',
