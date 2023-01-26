e50f55564dc8a8b0d230812bc377bd88cb2b358c
Hyunwoo Kim <admin@hyunwoo.kim>
2023-01-26 22:41:40 +0900

    Modify Mongoose Connector

--- src/hooks/mongoose.ts
+++ src/hooks/mongoose.ts
@@ -1,25 +1,58 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import mongoose from 'mongoose';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export function useMongoose(options?: mongoose.ConnectOptions) {
-  const connectOptions = {
-    serverSelectionTimeoutMS: process.env.VERCEL_ENV === 'development' ? 3000 : 10000,
-    retryWrites: true,
-    writeConcern: { w: 'majority' },
-    ...options,
-  } satisfies mongoose.ConnectOptions;
+export interface NextApiRequestWithMongoose extends NextApiRequest {
+  mongoose: typeof mongoose;
+}
 
-  const db = mongoose.connection;
+export declare type NextApiHandlerWithMongoose<T = any> = (
+  req: NextApiRequestWithMongoose,
+  res: NextApiResponse<T>
+) => void | Promise<void>;
 
-  mongoose.set('strictQuery', true);
-  mongoose.connect(process.env.MONGODB_URI!, connectOptions);
+// @ts-ignore
+let cachedMongoose = global.mongoose;
 
-  db.on('error', error => {
+if (!cachedMongoose) {
+  // @ts-ignore
+  cachedMongoose = global.mongoose = { connected: undefined, promise: undefined } as any;
+}
+
+export async function useMongoose(options?: mongoose.ConnectOptions) {
+  try {
+    if (cachedMongoose.connected) {
+      return cachedMongoose.connected;
+    }
+    if (!cachedMongoose.promise) {
+      const connectOptions = {
+        serverSelectionTimeoutMS: process.env.VERCEL_ENV === 'development' ? 3000 : 10000,
+        retryWrites: true,
+        writeConcern: { w: 'majority' },
+        ...options,
+      } satisfies mongoose.ConnectOptions;
+
+      mongoose.set('strictQuery', true);
+      cachedMongoose.promise = mongoose
+        .connect(process.env.MONGODB_URI!, connectOptions)
+        .then(m => m);
+    }
+    cachedMongoose.connected = await cachedMongoose.promise;
+    return cachedMongoose.connected;
+  } catch (error: any) {
     console.error(error);
     process.exit();
-  });
-  db.once('open', () => {
-    console.info('MongoDB Connected');
-  });
+  }
+}
 
-  return { db };
+export function withMongoose(
+  handler: NextApiHandlerWithMongoose,
+  options?: mongoose.ConnectOptions
+) {
+  return async (req: NextApiRequestWithMongoose, res: NextApiResponse): Promise<void> => {
+    const client = await useMongoose(options);
+
+    (req as NextApiRequestWithMongoose).mongoose = client;
+    return handler(req, res);
+  };
 }
