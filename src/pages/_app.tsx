e0be75a183fd86ba8f91ccaa688af1838b0add10
Hyunwoo Kim <admin@hyunwoo.kim>
2023-02-06 20:20:32 +0900

    Add React DnD

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { ChakraProvider, Progress } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Suspense } from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import type { AppProps } from 'next/app';
 
 const queryClient = new QueryClient({
@@ -23,8 +25,10 @@ export default function App({ Component, pageProps }: AppProps) {
       <ErrorBoundary fallback={<ErrorOverlay />}>
         <Suspense fallback={<Progress size="xs" isIndeterminate />}>
           <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-            <ReactQueryDevtools initialIsOpen={false} />
+            <DndProvider backend={HTML5Backend}>
+              <Component {...pageProps} />
+              <ReactQueryDevtools initialIsOpen={false} />
+            </DndProvider>
           </QueryClientProvider>
         </Suspense>
       </ErrorBoundary>
