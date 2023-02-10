b3bd36ce7370df5f32e3cb11a3e7bcee380d7ff0
Hyunwoo Kim <admin@hyunwoo.kim>
2023-02-11 02:55:18 +0900

    Add Google Map Callback

--- src/components/maps/GoogleMap.tsx
+++ src/components/maps/GoogleMap.tsx
@@ -19,11 +19,12 @@ export function GoogleMap() {
   return (
     <>
       <Script
-        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GCP_KEY}&v=weekly`}
+        src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GCP_KEY}&callback=c&v=weekly`}
         defer
         strategy="lazyOnload"
         onLoad={initMap}
       />
+      <Script id="googleMapCallback">{`function c(){}if(window){(window.c = c)}`}</Script>
       <Flex ref={mapRef} w="full" h="full" justifyContent="center" alignItems="center" />
     </>
   );
