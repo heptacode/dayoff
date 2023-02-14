1ef2c0209c2af92121c30e9ee052ea8d445d8c98
Hyunwoo Kim <admin@hyunwoo.kim>
2023-02-14 23:27:02 +0900

    Make selectedCollectionIds Persistable

--- src/stores/collectionStore.ts
+++ src/stores/collectionStore.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 import type { ICollection } from '@/types';
 
 export interface CollectionState {
@@ -20,38 +20,46 @@ export interface CollectionState {
 }
 
 export const useCollectionStore = create<CollectionState>()(
-  devtools((set, get) => ({
-    isLoading: true,
-    isEditing: false,
-    collections: new Map(),
-    collectionId: '',
-    selectedCollectionIds: [],
-    updatedAt: new Date(),
-    getCollections: () => [...get().collections.values()],
-    getSelectedCollections: () =>
-      get()
-        .getCollections()
-        .filter(collection => get().selectedCollectionIds.includes(collection._id)),
-    setIsLoading(value) {
-      set({ isLoading: value });
-    },
-    setIsEditing(value) {
-      set({ isEditing: value });
-    },
-    setCollections(collectionId, value) {
-      set({ collections: this.collections.set(collectionId, value) });
-      set({ updatedAt: new Date() });
-    },
-    clearCollections() {
-      set({ collections: new Map() });
-      set({ updatedAt: new Date() });
-    },
-    setCollectionId(value) {
-      set({ collectionId: value });
-    },
-    setSelectedCollectionIds(value) {
-      set({ selectedCollectionIds: value });
-      set({ updatedAt: new Date() });
-    },
-  }))
+  persist(
+    devtools((set, get) => ({
+      isLoading: true,
+      isEditing: false,
+      collections: new Map(),
+      collectionId: '',
+      selectedCollectionIds: [],
+      updatedAt: new Date(),
+      getCollections: () => [...get().collections.values()],
+      getSelectedCollections: () =>
+        get()
+          .getCollections()
+          .filter(collection => get().selectedCollectionIds.includes(collection._id)),
+      setIsLoading(value) {
+        set({ isLoading: value });
+      },
+      setIsEditing(value) {
+        set({ isEditing: value });
+      },
+      setCollections(collectionId, value) {
+        set({ collections: this.collections.set(collectionId, value) });
+        set({ updatedAt: new Date() });
+      },
+      clearCollections() {
+        set({ collections: new Map() });
+        set({ updatedAt: new Date() });
+      },
+      setCollectionId(value) {
+        set({ collectionId: value });
+      },
+      setSelectedCollectionIds(value) {
+        set({ selectedCollectionIds: value });
+        set({ updatedAt: new Date() });
+      },
+    })),
+    {
+      name: 'collection',
+      partialize: state => ({
+        selectedCollectionIds: state.selectedCollectionIds,
+      }),
+    }
+  )
 );
