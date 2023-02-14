a3946681d00f1c83dae695c669f6b03afc0a79cb
Hyunwoo Kim <admin@hyunwoo.kim>
2023-02-14 22:16:38 +0900

    Add Meta Tag to Prevent Zoom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import '@/styles/globals.css';
 import { ChakraProvider, Progress } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import Head from 'next/head';
 import { Suspense } from 'react';
 import type { AppProps } from 'next/app';
 
@@ -19,15 +20,21 @@ const queryClient = new QueryClient({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <ErrorBoundary fallback={<ErrorOverlay />}>
-        <Suspense fallback={<Progress size="xs" isIndeterminate />}>
-          <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-            <ReactQueryDevtools initialIsOpen={false} />
-          </QueryClientProvider>
-        </Suspense>
-      </ErrorBoundary>
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      </Head>
+
+      <ChakraProvider theme={theme}>
+        <ErrorBoundary fallback={<ErrorOverlay />}>
+          <Suspense fallback={<Progress size="xs" isIndeterminate />}>
+            <QueryClientProvider client={queryClient}>
+              <Component {...pageProps} />
+              <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+          </Suspense>
+        </ErrorBoundary>
+      </ChakraProvider>
+    </>
   );
 }
